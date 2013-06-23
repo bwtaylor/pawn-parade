e8bcbfa43f9fe88cf62c8e11d4f21e35c51ffb2e
Bryan Taylor <btaylor@rackspace.com>
2013-06-22 19:28:10 -0500

    basic rails, rspec, cucumber, webrat starter project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PawnParade::Application.config.secret_token = '77db73bbbc0c80600f64b89eae025ce326d3503d28aede0f3092e27c349227df548af6320f2a8a9e338384c22795e0181b720ade14f7c64d48262502da076214'
